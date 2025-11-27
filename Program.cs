f7e04f3cc04fecb31ca6ef39918ea32435fd27fa
PR3S1G4 <153407926+PR3S1G4zZ@users.noreply.github.com>
2025-11-27 09:40:22 -0500

    Change greeting message to Spanish

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World from minimal API!");
-
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-app.Urls.Add($"http://0.0.0.0:{port}");
+app.MapGet("/", () => "¡Hola Mundo desde .NET 8 en Docker!");
 
 app.Run();
